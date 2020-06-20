4f9eb945bde1008f93a7e4fc8db96f29fba5ad51
Alex <part.n@yandex.ru>
2020-06-21 00:24:00 +0300

    logic new quiz

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -71,8 +71,14 @@ class Auth extends Component {
 
         formControls[controlName] = control;
 
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
         this.setState({
-            formControls
+            formControls, isFormValid
         })
     };
 
